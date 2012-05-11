c6a9694b246e1d1770e3270240af08cf1c8318aa
Mindaugas Bujanauskas <mb@apollomusic.dk>
2012-05-11 09:42:55 +0200

    Added Basicmodel class to repo

---
+++ Basicmodel.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+#
+#     CodeIgniter Basic Model class
+#     (c) 2012 Mindaugas Bujanauskas, Apollo Music Aps
+#
+
+#
+# Basic Model is a class for CodeIgniter that simplifies and automates your workflow when
+# working with your data models in CodeIgniter.
+#
+
+class Basicmodel
+{
+    
+    
+    
+}
