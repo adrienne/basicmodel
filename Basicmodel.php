8ac82076c10e283bf4a99a5eb87974d5e9ad77a8
Mindaugas Bujanauskas <mb@apollomusic.dk>
2012-05-11 10:24:49 +0200

    Added workflow documentation

--- Basicmodel.php
+++ Basicmodel.php
@@ -10,6 +10,35 @@
 # working with your data models in CodeIgniter.
 #
 
+#
+# In your model:
+#
+#    class Mymodel extends Basicmodel
+#    {
+#       
+#    }
+#
+# In your controller:
+#
+#    $this->load->model('mymodel_model', 'mymodel');
+#    $mymodel = $this->mymodel->new();
+#    $mymodel->name = 'My name';
+#    $mymodel->save();
+#
+# Or, in your controller:
+#
+#    $this->load->model('mymodel_model', 'mymodel');
+#    $mymodels = $this->mymodel->all();
+#
+#    $this->load->view('myview', array('models' => $mymodels))
+#
+# And then in your view:
+#
+#    foreach($models as $model):
+#       echo $model->name;
+#    endforeach;
+#
+
 class Basicmodel
 {
     
