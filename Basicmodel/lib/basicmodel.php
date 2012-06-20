c861a99c93a500dd5c0e8a2a2db3c02c57cf438e
Mindaugas Bujanauskas <bujanauskas.m@gmail.com>
2012-06-20 23:06:26 +0200

    Starting on development

--- Basicmodel/lib/basicmodel.php
+++ Basicmodel/lib/basicmodel.php
@@ -2,13 +2,11 @@
 
 /**
  * @package   Basicmodel
- * @version   0.0.1
  * @author    Mindaugas Bujanauskas <bujanauskas.m@gmail.com>
  * @copyright (c) 2012 Mindaugas Bujanauskas
  */
 
-// In case Basicmodel is auto-loaded, we need to load `model` class first.
-load_class('model', 'core');
-
-// Load Basicmodel files
-require_once('lib/basicmodel.php');
+class Basicmodel extends CI_Model
+{
+    
+}
