2b886ebfa16c471707de65e6e98db450abf75b68
Mindaugas Bujanauskas <mb@apollomusic.dk>
2012-06-25 11:36:54 +0200

    Added a version number to the file. Time to start using 'Search and replace all files' functionality of my editor

--- Basicmodel/lib/basicmodel.php
+++ Basicmodel/lib/basicmodel.php
@@ -2,6 +2,7 @@
 
 /**
  * @package   Basicmodel
+ * @version   0.0.1
  * @author    Mindaugas Bujanauskas <bujanauskas.m@gmail.com>
  * @copyright (c) 2012 Mindaugas Bujanauskas
  * @license   http://www.opensource.org/licenses/mit-license.php/ MIT
