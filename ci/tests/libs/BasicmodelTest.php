ff59dfbf6975c7c6b6347c9c620a26626eb67dc4
Mindaugas Bujanauskas <mb@apollomusic.dk>
2012-06-27 10:45:07 +0200

    Added some more tests

--- ci/tests/libs/BasicmodelTest.php
+++ ci/tests/libs/BasicmodelTest.php
@@ -12,28 +12,71 @@ class BasicmodelTest extends CIUnit_TestCase
         $this->model = new Basicmodel();
     }
 
+    /**
+     * Should be instance of Basicmodel
+     */
     public function testNewInstance()
     {
         $this->assertInstanceOf('Basicmodel', $this->model);
     }
 
+    /**
+     * Class should have correct attributes
+     */
     public function testHasAttributes()
     {
         $this->assertClassHasAttribute('attributes', 'Basicmodel');
     }
 
+    /**
+     * Class should have correct static attributes
+     */
     public function testHasStaticAttributes()
     {
         $this->assertClassHasStaticAttribute('table', 'Basicmodel');
         $this->assertClassHasStaticAttribute('key', 'Basicmodel');
     }
 
+    /**
+     * Class should have correct default attribute values
+     */
+    public function testClassAttributesHaveCorrectValues()
+    {
+        $this->assertEquals('id', Basicmodel::$key);
+        $this->assertTrue(empty(Basicmodel::$table));
+    }
+
+    /**
+     * Instance method `is_new()` should behave properly
+     * 
+     * - It should return `TRUE` if the model does not have value assigned to its key
+     * - It should return `FALSE` if the model has value assigned to its key
+     */
     public function testIsNew()
     {
-        $this->assertTrue($this->model->is_new(), 'When key not set, should be TRUE');
+        $this->assertTrue($this->model->is_new());
+
+        $this->model->{Basicmodel::$key} = null;
+        $this->assertTrue($this->model->is_new());
 
         $this->model->{Basicmodel::$key} = 10;
-        $this->assertFalse($this->model->is_new(), 'When key is set, should be FALSE');
+        $this->assertFalse($this->model->is_new());
+    }
+
+    /**
+     * Instance method `table()` should behave properly
+     *
+     * - It should return model name in plural, if static `$table` is not set
+     * - It should return static `$table` if it is set
+     *
+     * @todo It should return correct plural form (person => people, money => money)
+     */
+    public function testTable()
+    {
+        $this->assertEquals('basicmodels', $this->model->table());
+
+        Basicmodel::$table = 'users';
+        $this->assertEquals('users', $this->model->table());
     }
     
 }
